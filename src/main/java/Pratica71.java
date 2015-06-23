e3b716c7ea7692fbd296cd1ea9b223aa616201e8
a1590677 <a1590677@localhost>
2015-06-23 11:49:40 -0300

    first commit

---
+++ src/main/java/Pratica71.java
@@ -0,0 +1,105 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import static java.util.Collections.list;
+import java.util.Scanner;
+import utfpr.ct.dainf.if62c.pratica.Jogador;
+import utfpr.ct.dainf.if62c.pratica.JogadorComparator;
+import utfpr.ct.dainf.if62c.pratica.Time;
+
+/**
+ * IF62C Fundamentos de Programação 2 Exercício de programação em Java.
+ *
+ * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
+ */
+public class Pratica71 {
+
+    public static void main(String[] args) {
+        int numero = 0;
+        int quer = 0;
+        ArrayList<Jogador> timeco = new ArrayList<Jogador>();
+        Time time = new Time();
+        Scanner scanner = new Scanner(System.in);
+        String nome1 = null;
+        int numJogadores = 0;
+        System.out.print("Número de Jogadores: ");
+        if (scanner.hasNextInt()) {
+            numJogadores = scanner.nextInt();
+        } else {
+            System.out.println("Digite um número válido!");
+        }
+        for (int i = 0; i < numJogadores; i++) {
+            System.out.println("Digite a posicao do " + i + " jogador:  ");
+            if (scanner.hasNext()) {
+                nome1 = scanner.next();
+            } else {
+                System.out.println("Digite um nome!");
+            }
+            System.out.println("Digite o numero do " + i + " jogador:  ");
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
+            } else {
+                System.out.println("Digite um numero!");
+            }
+            timeco.add(new Jogador(numero, nome1));
+
+        }
+        timeco.sort(new JogadorComparator(true, true, true));
+
+        String format = "%3d %-20s";
+        System.out.println("Time ordenado por número (asc) e nome (desc)");
+        System.out.println("Num Nome");
+        for (Jogador j : timeco) {
+            System.out.println(String.format(format, j.getNumero(), j.getNome()));
+        }
+
+        System.out.println("se deseja incluir mais jogadore digite 1, caso contrario digite 0");
+        if (scanner.hasNextInt()) {
+            quer = scanner.nextInt();
+        } else {
+            System.out.println("Digite um numero!");
+        }
+        while (quer == 1) {
+            System.out.println("Digite o numero do  jogador:  ");
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
+            } else {
+                System.out.println("Digite um numero!");
+            }
+            if(numero==0){
+                break;
+            }
+            
+            System.out.println("Digite a posicao do jogador:  ");
+            if (scanner.hasNext()) {
+                nome1 = scanner.next();
+            } else {
+                System.out.println("Digite um nome!");
+            }
+            
+            timeco.add(new Jogador(numero, nome1));
+            timeco.sort(new JogadorComparator(true, true, true));
+        }
+        
+        System.out.println("Time ordenado por número (asc) e nome (desc)");
+        System.out.println("Num Nome");
+        for (Jogador j : timeco) {
+            System.out.println(String.format(format, j.getNumero(), j.getNome()));
+        }
+        // para buscar por número é necessário reordenar
+       /* JogadorComparator comparador = new JogadorComparator();
+         jogadores.sort(comparador);
+         System.out.println("Time ordenado por número (asc) e nome (asc)");
+         System.out.println("Num Nome");
+         for (Jogador j : jogadores) {
+         System.out.println(String.format(format, j.getNumero(), j.getNome()));
+         }
+         int pos = Collections.binarySearch(jogadores, new Jogador(10, "Beltrano"), comparador);
+         if (pos >= 0) {
+         System.out.print("Jogador encontrado: ");
+         System.out.println(jogadores.get(pos));
+         } else {
+         System.out.println("Jogador não encontrado. Posição: " + (-pos - 1));
+         }*/
+    }
+}
